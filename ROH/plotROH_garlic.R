c736a6d6565d961bee080259b8b6235cf34b8536
jaam92 <jazlyn.mooney@gmail.com>
2021-08-25 17:25:42 -0700

    all ROH calculations

--- ROH/plotROH_garlic.R
+++ ROH/plotROH_garlic.R
@@ -4,10 +4,10 @@ library(data.table)
 library(ggpubr)
 
 #Load ibdseq roh outputs and meta data
-setwd("~/Documents/TigerProject/Rscripts/ROH/")
+setwd("~/TigerProject/ROH/")
 
-popsDF = read_csv("~/TigerProject/individual_ids.csv")
-
+popsDF = read_csv("~/TigerProject/IndivFiles/individual_ids.csv") %>%
+  mutate(combo = ifelse(Subspecies == "Unknown", paste(Subspecies, "-", FROH$Phenotype, sep = ""), Subspecies))
 
 roh = read_delim("TrueROH_propCoveredwithin1SDMean_gr100kb_allChroms_highCov_runSpeciesSep_garlic.txt", delim = "\t") %>%
   left_join(popsDF,by = c("INDV" = "Individual"))
@@ -23,7 +23,7 @@ FROH = rohLengthsClass %>%
   mutate(Froh = AUTO_LEN/2174711735)
 
 ##Plot data
-cbPalette = c("Generic" = "gray25", "Generic* (Amur)" = "lightsalmon2" , "Generic* (Bengal)" = "lightskyblue2", "Amur" = "#D55E00",  "Bengal" = "steelblue", "Malayan" = "#009E73", "Sumatran" = "gold3", "Indochinese" = "chocolate1", "Sanctuary" = "#CC79A7", "F1Wild" = "#867BCF", "Wild"="goldenrod2", "F2Wild"="coral2", "Captive"="cornflowerblue", "Unknown"="darkmagenta", "Zoo"="darkseagreen3", "South China" = "firebrick2") #palette
+cbPalette = c("Generic" = "gray25", "Generic* (Amur)" = "lightsalmon2" , "Generic* (Bengal)" = "lightskyblue2", "Amur" = "#D55E00",  "Bengal" = "steelblue", "Malayan" = "#009E73", "Sumatran" = "gold3", "Indochinese" = "chocolate1", "South China" = "firebrick2", "Unknown"="darkmagenta", "Unknown-Orange" = "#CC79A7", "Unknown-SnowWhite" = "#867BCF", "Unknown-Golden"="darkseagreen3", "Unknown-White"="cornflowerblue") #palette
 
 ggplot(rohLengthsClass, aes(x=Subspecies, y=AUTO_LEN/10^6, fill=Subspecies)) + 
   geom_boxplot() +
@@ -47,4 +47,19 @@ ggplot(FROH, aes(x=Subspecies, y=Froh, colour=Subspecies)) +
   theme(axis.text.x = element_text(hjust = 0.5, vjust = 0.5, size = 16), 
         axis.text.y = element_text(size = 16), 
         plot.title = element_text(size = 18, face = "bold", hjust = 0.5), 
-        axis.title = element_text(size = 16))
+        axis.title = element_text(size = 16),
+        legend.title = element_text(size=16),
+        legend.text = element_text(size=14))
+
+ggplot(FROH, aes(x=Subspecies, y=Froh, colour=Subspecies)) +
+  geom_boxplot(size=1) + 
+  geom_point(size=0.5) + 
+  scale_colour_manual(name = "Sub-species", values = cbPalette) + 
+  labs(x = "Sub-species", y=expression(F[ROH])) + 
+  theme_bw() + 
+  theme(axis.text.x = element_text(hjust = 0.5, vjust = 0.5, size = 16), 
+        axis.text.y = element_text(size = 16), 
+        plot.title = element_text(size = 18, face = "bold", hjust = 0.5), 
+        axis.title = element_text(size = 16),
+        legend.title = element_text(size=16),
+        legend.text = element_text(size=14))
