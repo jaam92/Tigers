5dfd8b86b15ce0ece1e11f2e1cb402eaa1804b98
jaam92 <jazlyn.mooney@gmail.com>
2021-10-08 13:36:05 -0700

    added a summary table and fixed column error

--- ROH/plotROH_garlic.R
+++ ROH/plotROH_garlic.R
@@ -6,8 +6,7 @@ library(ggpubr)
 #Load ibdseq roh outputs and meta data
 setwd("~/TigerProject/ROH/")
 
-popsDF = read_csv("~/TigerProject/IndivFiles/individual_ids.csv") %>%
-  mutate(combo = ifelse(Subspecies == "Unknown", paste(Subspecies, "-", FROH$Phenotype, sep = ""), Subspecies))
+popsDF = read_csv("~/TigerProject/IndivFiles/individual_ids.csv")
 
 roh = read_delim("TrueROH_propCoveredwithin1SDMean_gr100kb_allChroms_highCov_runSpeciesSep_garlic.txt", delim = "\t") %>%
   left_join(popsDF,by = c("INDV" = "Individual"))
@@ -21,9 +20,21 @@ rohLengthsClass = roh %>%
 FROH = rohLengthsClass %>%
   filter(TYPE == "C") %>%
   mutate(Froh = AUTO_LEN/2174711735)
+summaryTable = roh %>%
+  arrange(TYPE, Subspecies) %>%
+  group_by(TYPE, Subspecies) %>%
+  summarise(mean = mean(AUTO_LEN)/10^3, 
+            min = min(AUTO_LEN)/10^3, 
+            max = max(AUTO_LEN)/10^3) %>%
+  mutate_if(is.numeric, round, digits=3) %>%
+  ungroup() %>%
+  rename("ROH class" = TYPE, "subspecies" = "Subspecies")
 
 ##Plot data
 cbPalette = c("Generic" = "gray25", "Generic* (Amur)" = "lightsalmon2" , "Generic* (Bengal)" = "lightskyblue2", "Amur" = "#D55E00",  "Bengal" = "steelblue", "Malayan" = "#009E73", "Sumatran" = "gold3", "Indochinese" = "chocolate1", "South China" = "firebrick2", "Unknown"="darkmagenta", "Unknown-Orange" = "#CC79A7", "Unknown-SnowWhite" = "#867BCF", "Unknown-Golden"="darkseagreen3", "Unknown-White"="cornflowerblue") #palette
+
+
+ggtexttable(summaryTable, rows = NULL, theme = ttheme("mBlackWhite"))
 
 ggplot(rohLengthsClass, aes(x=Subspecies, y=AUTO_LEN/10^6, fill=Subspecies)) + 
   geom_boxplot() +
