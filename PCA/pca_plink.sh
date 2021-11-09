e348a737a7ac1ff5a0eaf60ddf952325b44a5d80
ellieearmstrong <ellieearmstrong@gmail.com>
2021-11-09 10:44:02 -0700

    Update pca_plink.sh

--- PCA/pca_plink.sh
+++ PCA/pca_plink.sh
@@ -5,7 +5,8 @@ plink2 --vcf highcov-nofilter-biallelic-AN-MM-pcc.vcf --make-bed --allow-extra-c
 #run PCs
 plink2 --bfile highcov-nofilter-biallelic-AN-MM-pcc --allow-extra-chr --pca 10 --out highcov-nofilter-biallelic-AN-MM-pcc --threads 4
 
-
 #build pca for all uncorrected individuals
+plink2 --vcf highcov-lowcov-biallelic-AN-MM-pcc.vcf.gz --make-bed --allow-extra-chr --out highcov-lowcov-biallelic-AN-MM-pcc
+plink2 --bfile highcov-lowcov-biallelic-AN-MM-pcc --allow-extra-chr --pca 10 --out highcov-lowcov-biallelic-AN-MM-pcc --threads 4
 
 #build pca for all corrected individuals
