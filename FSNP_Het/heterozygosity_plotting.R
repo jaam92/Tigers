748077463f0a092dd11a079fc1677a9677f48d44
jaam92 <jazlyn.mooney@gmail.com>
2022-12-01 22:20:06 -0800

    updated meta data col names

--- FSNP_Het/heterozygosity_plotting.R
+++ FSNP_Het/heterozygosity_plotting.R
@@ -4,21 +4,18 @@ options(scipen = 999) #use this to turn off scientific notation
 setwd("~/Documents/Tigers/FSNP_Het")
 
 ####Load files
-heterozygosity = read_delim('~/Documents/Tigers/FSNP_Het/allSamp.het', delim = '\t', col_names = TRUE)
-colnames(heterozygosity) = c('Individual','O_HOM', 'E_HOM', 'N_SITES','FSNP') #rename columns
+heterozygosity = read_delim('~/Documents/Tigers/FSNP_Het/allSamp_Nov2022.het', delim = '\t', col_names = TRUE)
+colnames(heterozygosity) = c('Sample','O_HOM', 'E_HOM', 'N_SITES','FSNP') #rename columns
 
-popsDF = read_csv("~/Documents/Tigers/IndivFiles/individual_ids.csv") %>%
-  mutate(combo = ifelse(Subspecies2 == "Generic", paste(Subspecies2, "-", Phenotype, sep = ""), Subspecies2))
+popsDF = read_delim("~/Documents/Tigers/IndivFiles/TableX-SampleDetails.txt")
 
 ####Make plotting data frame 
 all_nodups_full_highCov = heterozygosity %>%
   left_join(popsDF) %>%
-  filter(!Individual %in% indivsToRemove) %>%
   mutate(Heterozygosity = (N_SITES-O_HOM)/1476111759,
-         Subspecies2 = factor(Subspecies2, levels = c('Generic', 'Amur', 'Bengal', 'Indochinese', 'Malayan', 'South China', 'Sumatran')),
-         BoundFSNP = ifelse(FSNP < 0, 0, FSNP),
-         Subspecies = NULL) %>%
-  arrange(Subspecies2, Heterozygosity) 
+         Subspecies = factor(Subspecies_GroupID, levels = c('Generic', 'Amur', 'Bengal', 'Indochinese', 'Malayan', 'South China', 'Sumatran')),
+         BoundFSNP = ifelse(FSNP < 0, 0, FSNP)) %>%
+  arrange(Subspecies, Heterozygosity) 
 
 
 ####Plot heterozygosity all-nodups ------------------------------------------
@@ -26,12 +23,12 @@ cbPalette = c("Amur" = "#0072B2",  "Bengal" = "#882255", "Malayan" = "#009E73", 
 cbPalette_expanded = c("Amur" = "#0072B2",  "Bengal" = "#882255", "Malayan" = "#009E73", "Sumatran" = "cornflowerblue", "Indochinese" = "gold4", "South China" = "plum", "Generic"="gray25", "Generic-Orange" = "#CC79A7", "Generic-SnowWhite" = "#867BCF", "Generic-Golden"="darkseagreen3", "Generic-White"="cornflowerblue")#palette
 
 #order individuals
-IDs = all_nodups_full_highCov$Individual 
-all_nodups_full_highCov$Individual = factor(all_nodups_full_highCov$Individual, levels = IDs)
+IDs = all_nodups_full_highCov$Sample 
+all_nodups_full_highCov$Sample = factor(all_nodups_full_highCov$Sample, levels = IDs)
 
 #lollipop plot
-LolliPlotHet = ggplot(all_nodups_full_highCov, aes(x=Individual, y=Heterozygosity, colour = Subspecies2)) + 
-  geom_segment(aes(x=Individual, xend=Individual, y=0, yend=Heterozygosity), size = 1, colour = "grey")+
+LolliPlotHet = ggplot(all_nodups_full_highCov, aes(x=Sample, y=Heterozygosity, colour = Subspecies)) + 
+  geom_segment(aes(x=Sample, xend=Sample, y=0, yend=Heterozygosity), size = 1, colour = "grey")+
   geom_point(size = 2) +
   scale_color_manual(name="Subspecies", values=cbPalette) +
   labs(x="Individual", y="Observed Heterozygosity") +
@@ -48,8 +45,8 @@ LolliPlotHet = ggplot(all_nodups_full_highCov, aes(x=Individual, y=Heterozygosit
 
 #violin plots
 #jpeg(file="heterozygosity-vcftools-calledPerSubspecies-nodups-high-cov.jpeg", width = 1100, height = 550, res = 100)
-VioPlotHet = ggplot(all_nodups_full_highCov, aes(x=Subspecies2, y=Heterozygosity)) +
-  geom_violin(aes(fill=Subspecies2)) + 
+VioPlotHet = ggplot(all_nodups_full_highCov, aes(x=Subspecies, y=Heterozygosity)) +
+  geom_violin(aes(fill=Subspecies)) + 
   geom_jitter(height = 0, width = 0.1) +
   scale_fill_manual(name = "Subspecies", values = cbPalette) + 
   labs(x="Subspecies",  y="Observed Heterozygosity") + 
@@ -65,8 +62,8 @@ VioPlotHet = ggplot(all_nodups_full_highCov, aes(x=Subspecies2, y=Heterozygosity
 
 ####Plot FSNP
 #jpeg(file="FSNP_vioplot.jpeg", width = 1040, height = 700, res = 100)
-plotFSNP = ggplot(all_nodups_full_highCov, aes(x=Subspecies2, y=FSNP)) +
-  geom_violin(aes(fill=Subspecies2)) + 
+plotFSNP = ggplot(all_nodups_full_highCov, aes(x=Subspecies, y=FSNP)) +
+  geom_violin(aes(fill=Subspecies)) + 
   geom_jitter(height = 0, width = 0.1) +
   scale_y_continuous(breaks=seq(0,1,0.1)) +
   scale_fill_manual(name = "Subspecies", values = cbPalette) + 
