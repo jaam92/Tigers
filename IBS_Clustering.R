3b9e79c69237292d48c711330a92003af3b36fbe
Jazlyn Mooney <jazlynmo@dts.usc.edu>
2023-03-01 14:51:38 -0800

    plot fig 1 ibs tree

--- IBS_Clustering.R
+++ IBS_Clustering.R
@@ -10,15 +10,16 @@ library(seriation) #olo function
 library(cluster) #use silhouette
 
 #Set working directory and load files
-setwd("~/Documents/Tigers/PCA/")
+setwd("~/Tigers/PCA/plinkFiles")
 set.seed(10192021)
 
-#Open files and prep
-popsDF = read_delim("~/Documents/Tigers/IndivFiles/TableX-SampleDetails.txt") 
+#Open files
+ancestryInfo = read_csv("~/Tigers/IndivFiles/plotting_metadata.csv")
+popsDF = read_delim("~/Tigers/IndivFiles/TableX-SampleDetails.txt") 
+gds = snpgdsOpen("~/Tigers/PCA/plinkFiles/highcov.nodups.highcov.biallelic.AN.pCC.DPQUALtest.genmap.gds") #open genofile
 
-gds = snpgdsOpen("~/Documents/Tigers/PCA/plinkFiles/highcov.nodups.highcov.biallelic.AN.pCC.DPQUALtest.genmap.gds") #open genofile
+#Get sample and family ids
 sampIds = read.gdsn(index.gdsn(gds, "sample.id")) #grab sample ids 
-
 famIds = as.data.frame(sampIds) %>%
   left_join(popsDF, by = c("sampIds"="Sample")) %>%
   pull(Subspecies_GroupID_Corrected)#make family ids
@@ -30,80 +31,45 @@ captive = popsDF %>%
   pull(Sample)
 samp.id.captive = unlist(gdsSampIDs[gdsSampIDs%in%captive])
 
-
-
 #LD prune because data set is small, set r2 = 0.7, maf 5%, and 50 snp window 
 snpset = snpgdsLDpruning(gds, sample.id=samp.id.captive, method="corr", slide.max.n=50, ld.threshold=0.3, maf = 0.05, autosome.only = F) 
 pruned.captive = unlist(snpset, use.names=FALSE)
-
-
-#Run KING
-KING = snpgdsIBDKING(gds, sample.id = samp.id.captive, snp.id=pruned.captive, autosome.only = F, num.thread = 4)
-
-#Make the IBS0 (probability IBS=0) matrix
-kingIBSMatrix = KING$IBS0
-colnames(kingIBSMatrix) <- rownames(kingIBSMatrix) <- KING$sample.id
-disMatrix = as.dist(kingIBSMatrix) #convert to distance matrix
-
-#Perform heirarchical clustering
-hc_avg = hclust(disMatrix, method = "average")
-hc_avg_reorder = reorder(hc_avg, disMatrix, method = "OLO") #reorder with optimal leaf ordering
-
-#Function to plot 
-dend = hc_avg_reorder %>% 
-  as.dendrogram %>%
-  #set("branches_k_color", k = 1, c("#0072B2", "#882255", "darkseagreen4", "gray25", "gold4", "#D55E00")) %>% 
-  set("branches_lwd", 0.7) %>%
-  set("labels_cex", 1.5) %>% 
-  #set("labels_colors", k = 1, c("#0072B2", "#882255", "darkseagreen4", "gray25", "gold4", "#D55E00")) %>%
-  set("leaves_pch", 19) %>% 
-  set("leaves_cex", 0.5) 
-
-ggd1 = as.ggdend(dend)
-
-IBSTree = ggplot(ggd1, horiz = F) + 
-  theme(axis.text.x = element_blank(), 
-        axis.text.y = element_text(size = 30),
-        axis.title=element_text(size = 30))
 
 #SNPRelate IBS 
 snpgdsIBS = snpgdsIBS(gds, sample.id = samp.id.captive, snp.id=pruned.captive, autosome.only = F, num.thread=4)
 snpgdsIBSMatrix = snpgdsIBS$ibs
 colnames(snpgdsIBSMatrix) <- rownames(snpgdsIBSMatrix) <- snpgdsIBS$sample.id
-ibs.hc = snpgdsHCluster(snpgdsIBS)
-snpgdsClose(gds)
+ibs.hc = snpgdsHCluster(snpgdsIBS) #cluster based on IBS
+snpgdsClose(gds) #close file
  
- 
-#IBS distance matrix from snpgds 
-rv = snpgdsCutTree(ibs.hc)
-IBSTreeSNPgds = ggplot(rv$dendrogram, horiz = F) + 
-ggtitle("IBS Distance Matrix from SNP Relate") + 
-  theme(axis.text.x = element_blank(), 
-         axis.text.y = element_text(size  = 20),
-         axis.title=element_text(size=24))
+#Plot as a dendrogram
+cbPalette = c("Amur" = "#0072B2",  "Bengal" = "#882255", "Malayan" = "#009E73", "Indochinese" = "gold4", "Sumatran" = "cornflowerblue", "South China" = "plum", "Generic"="gray25")#palette
 
-
-#Check whether cluster membership is well supported (Sw ~ 0.6, so it's pretty good)
-####For interpretation of silhouette width https://www.stat.berkeley.edu/~spector/s133/Clus.html
-####Code: https://www.rdocumentation.org/packages/cluster/versions/2.0.8/topics/silhouette
-
-c6 = c("tomato", "darkseagreen4", "steelblue", "purple2", "goldenrod4", "gray25")
-par(mfrow= c(3,2), oma= c(0,0, 3, 0),mgp= c(1.6,.8,0), mar= .1+c(4,2,2,2))
-for(k in 2:6){
-plot(silhouette(cutree(hc_avg_reorder, k= k),disMatrix), 
-     main = paste("k = ",k), 
-     do.n.k=FALSE, 
-     col = c6[1:k])
-}
-
-# Visualize silhouhette information
-require("cluster")
-sil <- silhouette(cutree(hc_avg_reorder, k=6),disMatrix)
-plot(sil, cex.names = par("cex.axis"))
+dend = dendro_data(ibs.hc$dendrogram) 
+labs = bind_cols(filter(segment(dend), x == xend & x%%1 == 0), label(dend)) %>%
+  select(xend, yend, label) %>%
+  mutate(ancestry = ancestryInfo$Top_Ancestry[match(label, ancestryInfo$Individual)]) 
+  
+#plot it 
+ggplot(segment(dend)) +
+  geom_segment(aes(x=x, y=y, xend=xend, yend=yend)) + 
+  geom_point(data=labs,
+             aes(x=xend, y=yend, colour=ancestry), size = 3) +
+  scale_colour_manual(values=cbPalette) +
+  coord_flip() + 
+  scale_y_reverse() +
+  theme(axis.line.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.title.y=element_blank(), 
+        axis.text.x=element_text(size = 20),
+        axis.title.x=element_text(size = 24),
+        panel.background=element_rect(fill="white"),
+        panel.grid=element_blank())
 
 
 ###All individuals
-setwd("~/Documents/Tigers/PCA/")
+setwd("~/Tigers/PCA/")
 
 gds = snpgdsOpen("high-corr-nodups-biallelic-AN-MM-pcc.gds") #open genofile
 sampIds = read.gdsn(index.gdsn(gds, "sample.id")) #grab sample ids 
@@ -122,11 +88,3 @@ KING = snpgdsIBDKING(gds, sample.id = sampIds, snp.id=pruned, autosome.only = F)
 kingIBSMatrix = KING$IBS0
 colnames(kingIBSMatrix) <- rownames(kingIBSMatrix) <- KING$sample.id
 disMatrix = as.dist(kingIBSMatrix) #convert to distance matrix
-
-
-#SNPRelate IBS 
-snpgdsIBS = snpgdsIBS(gds, sample.id = sampIds, snp.id=pruned, num.thread=4)
-snpgdsIBSMatrix = snpgdsIBS$ibs
-colnames(snpgdsIBSMatrix) <- rownames(snpgdsIBSMatrix) <- snpgdsIBS$sample.id
-ibs.hc = snpgdsHCluster(snpgdsIBS)
-snpgdsClose(gds)
